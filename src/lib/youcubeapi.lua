4d355775ee6aef1016b20ab618171f9f6b66a3de
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-08-20 06:11:57 +0300

    Added my own server URL

--- src/lib/youcubeapi.lua
+++ src/lib/youcubeapi.lua
@@ -33,6 +33,7 @@ end
 -- Contact the server owner on Discord, when the server is down
 local servers = {
     "ws://127.0.0.1:5000", -- Your server!
+    "wss://youcube-server-ixd1.onrender.com",  -- By FlooferLand
     "wss://youcube.knijn.one", -- By EmmaKnijn#0043
     "wss://youcube.onrender.com", -- By Commandcracker#8528
 }
